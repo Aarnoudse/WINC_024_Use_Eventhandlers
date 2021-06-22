5d00c0f4a650b72b7cbadaa2abe94970f18a4386
Aarnoudse <83244265+Aarnoudse@users.noreply.github.com>
2021-06-22 20:26:59 +0200

    Update script.js

--- WINC_024_Use_Eventhandlers_defer/script.js
+++ WINC_024_Use_Eventhandlers_defer/script.js
@@ -1,5 +1,21 @@
-function onclick () {
-    var button = document.getElementsByClassName("geenidee").button;
-    document.getElementsByClassName('btn-alert').button.button;
-    alert(button)
-}
+const attachEventToAlertButton = function () {
+    const alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click", function () {
+        alert("Hello World!");
+    });
+};
+
+const toggleColor = function () {
+    const bodyElement = document.body;
+    bodyElement.classList.toggle("red-background");
+};
+
+const attachEventToChangeColorButton = function () {
+    const changeColorButton = document.getElementById("change-background-button");
+    changeColorButton.addEventListener("click", function () {
+        toggleColor();
+    });
+};
+
+attachEventToAlertButton();
+attachEventToChangeColorButton();
